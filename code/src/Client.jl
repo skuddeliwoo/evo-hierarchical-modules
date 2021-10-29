f0cb99715d1e79819bf22e6c11f9ad67cc42c61c
Andreas Rohrbach <skuddeliwoo@gmail.com>
2021-10-29 11:47:51 +0200

    finish changeEnv

--- code/src/Client.jl
+++ code/src/Client.jl
@@ -8,36 +8,37 @@ nEpisodes = 1000 # lit: 100000
 
 for episode in 1:nEpisodes
     # init environment
+    env = changeEnv()
 
     # iterate over evo steps
 
     # iterate over dev steps
-
-    println(episode)
 end
 
 function changeEnv(z=0.95, nModules=nModules)
     envs = Dict(
-    "dPos" => Dict("coeffs"=>(0.7,   1), "prob"=>(p -> p < z/2)),
-    "dNeg" => Dict("coeffs"=>(  1, 0.7), "prob"=>(p -> p >= z/2 && p < z)),
-    "sPos" => Dict("coeffs"=>( -1,   1), "prob"=>(p -> p >= z && p < (z + (1-z) / 2))),
-    "sNeg" => Dict("coeffs"=>(  1,  -1), "prob"=>(p -> p >= (z + (1-z) / 2))),
+    "dPos" => Dict("coeffs"=>( 0.7,  1.0), "prob"=>(p -> p < z/2)),
+    "dNeg" => Dict("coeffs"=>( 1.0,  0.7), "prob"=>(p -> p >= z/2 && p < z)),
+    "sPos" => Dict("coeffs"=>(-1.0,  1.0), "prob"=>(p -> p >= z && p < (z + (1-z) / 2))),
+    "sNeg" => Dict("coeffs"=>( 1.0, -1.0), "prob"=>(p -> p >= (z + (1-z) / 2))),
     )
 
+    rolledEnvs = []
+
     for i in 1:nModules
-        println(i)
         roll = rand(Float64)
         if envs["dPos"]["prob"](roll)
-            println("i am dPos: $roll")
+            push!(rolledEnvs, envs["dPos"]["coeffs"])
         elseif envs["dNeg"]["prob"](roll)
-            println("i am dNeg: $roll")
+            push!(rolledEnvs, envs["dNeg"]["coeffs"])
         elseif envs["sPos"]["prob"](roll)
-            println("i am sPos: $roll")
+            push!(rolledEnvs, envs["sPos"]["coeffs"])
         elseif envs["sNeg"]["prob"](roll)
-            println("i am sNeg: $roll")
+            push!(rolledEnvs, envs["sNeg"]["coeffs"])
         end # if
-    end
+    end # for
+
+    println(rolledEnvs)
+    return rolledEnvs
 
 end
-
-changeEnv()
