63ec2ac03ed798fa1efddc16dbb04c76a9c12c81
root <root@h2947445.stratoserver.net>
2021-11-11 10:54:58 +0100

    fix: increase episodes from 100k to 200k

--- code/src/Params.jl
+++ code/src/Params.jl
@@ -4,5 +4,5 @@ nModules = 4
 nModuleSize = 4
 N = nModules * nModuleSize # Number of genes and traits
 individuals = [Individual(zeros(N), zeros(N,N))]
-nEpisodes = 100000 # lit: 100000
+nEpisodes = 200000 # lit: 200000
 nEvoSteps = 1000 # lit: 1000
